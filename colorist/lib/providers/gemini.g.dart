7bf7cbfb5d93ef5b82e40296575c19c02e9fb860
luckybossyt <panditlakshya22@gmail.com>
2025-10-06 22:32:41 +0530

    Initial commit with firebase configs

---
+++ colorist/lib/providers/gemini.g.dart
@@ -0,0 +1,79 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'gemini.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAppHash() => r'eaf72348ad852e52538678ec3a1b50b5c9af17ef';
+
+/// See also [firebaseApp].
+@ProviderFor(firebaseApp)
+final firebaseAppProvider = AutoDisposeFutureProvider<FirebaseApp>.internal(
+  firebaseApp,
+  name: r'firebaseAppProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseAppHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FirebaseAppRef = AutoDisposeFutureProviderRef<FirebaseApp>;
+String _$geminiModelHash() => r'c6550e4a62e99f22324928fae16928b7db5dce4a';
+
+/// See also [geminiModel].
+@ProviderFor(geminiModel)
+final geminiModelProvider = AutoDisposeFutureProvider<GenerativeModel>.internal(
+  geminiModel,
+  name: r'geminiModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geminiModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GeminiModelRef = AutoDisposeFutureProviderRef<GenerativeModel>;
+String _$chatSessionHash() => r'fdd5e4ed9d06db9712c9300eeb8a1b54a115b10a';
+
+/// See also [chatSession].
+@ProviderFor(chatSession)
+final chatSessionProvider = FutureProvider<ChatSession>.internal(
+  chatSession,
+  name: r'chatSessionProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$chatSessionHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ChatSessionRef = FutureProviderRef<ChatSession>;
+String _$geminiChatServiceHash() => r'62a58e95c6112ec2cc1b4777a88270e75fe2949f';
+
+/// See also [geminiChatService].
+@ProviderFor(geminiChatService)
+final geminiChatServiceProvider =
+    AutoDisposeProvider<GeminiChatService>.internal(
+      geminiChatService,
+      name: r'geminiChatServiceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$geminiChatServiceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GeminiChatServiceRef = AutoDisposeProviderRef<GeminiChatService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
